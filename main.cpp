bd42e360a61e91a7fb5d351fb5ecc2b402983877
Du Park <dupark3@gmail.com>
2018-02-24 20:35:29 -0500

    Prints map of words and occurance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
+// #include <algorithm> // sort
 #include <iostream>
 #include <map>
 #include <string>
+// #include <utility> // pair
 
 int main()
 {
+    std::string s;
+    std::map<std::string, int> counters; // store each word and an associated counter
+
+    // read the input, keeping track of each word and how often we see it
+    while (std::cin >> s)
+        ++counters[s];
+
+    // write the words and associated counts
+    for (std::map<std::string, int>::const_iterator it = counters.begin();
+         it != counters.end(); ++it){
+        std::cout << it->first << "\t" << it->second << std::endl;
+    }
 
     return 0;
 }
